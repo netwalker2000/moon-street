96484202809595682de9c0d25ee739cb441f3bbd
Zezhou Zheng <zezhou.zheng@shopee.com>
2021-07-09 09:10:48 +0800

    optimized logging info, concise and clear

--- cmd/main.go
+++ cmd/main.go
@@ -39,10 +39,14 @@ func main() {
 }
 
 func handleConnection(c net.Conn) {
+	var (
+		err_resp = []byte{0, 0, 0, 0}
+		pos_resp = []byte{0, 0, 0, 10, 123, 34, 99, 111, 100, 101, 34, 58, 48, 125}
+	)
 	header := make([]byte, 4)
 	if _, err := c.Read(header); err != nil {
 		log.Printf("error when read conn header, end this conn :%v", err)
-		c.Write([]byte{0, 0, 0, 0})
+		c.Write(err_resp)
 		return
 	}
 	bodyLen := binary.BigEndian.Uint32(header)
@@ -50,13 +54,13 @@ func handleConnection(c net.Conn) {
 	body := make([]byte, int(bodyLen))
 	if _, err := c.Read(body); err != nil {
 		log.Printf("error when read conn body, end this conn :%v", err)
-		c.Write([]byte{0, 0, 0, 0})
+		c.Write(err_resp)
 		return
 	}
 	var rData []common.RpcData
 	if err := json.Unmarshal(body, &rData); err != nil {
 		log.Printf("error when convert remote data, end this conn :%v", err)
-		c.Write([]byte{0, 0, 0, 0})
+		c.Write(err_resp)
 		return
 	}
 	defer func() {
@@ -69,9 +73,9 @@ func handleConnection(c net.Conn) {
 		//log.Printf("%v", v)
 		if err != nil {
 			log.Printf("error when exec No.%d in the batch :%v", i, err)
-			c.Write([]byte{0, 0, 0, 0})
+			c.Write(err_resp)
 			return
 		}
 	}
-	c.Write([]byte{0, 0, 0, 10, 123, 34, 99, 111, 100, 101, 34, 58, 48, 125})
+	c.Write(pos_resp)
 }
